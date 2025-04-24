f463feec1d865ba56b510766cdb1e597103d869c
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-04-24 16:43:02 -0700

    Update the analyzer

--- ds5678.Modding.TLD.SourceGenerator/MarkerAttributeAnalyzer.cs
+++ ds5678.Modding.TLD.SourceGenerator/MarkerAttributeAnalyzer.cs
@@ -10,8 +10,8 @@ public class MarkerAttributeAnalyzer : DiagnosticAnalyzer
 {
 	public const string DiagnosticId = "TLD_MODS_001";
 	private static readonly LocalizableString Title = "Mod class not partial";
-	private static readonly LocalizableString MessageFormat = "The ModClassAttribute should only be used on partial classes";
-	private static readonly LocalizableString Description = "MarkerAttribute is used in an unsupported location.";
+	private static readonly LocalizableString MessageFormat = "Classes with the ModClassAttribute should be partial";
+	private static readonly LocalizableString Description = "Classes with the ModClassAttribute need to have another partial declaration source generated, so it's invalid for classes with this attribute to not be marked as partial.";
 	private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, "Usage", DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
 
 	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
@@ -26,32 +26,34 @@ public class MarkerAttributeAnalyzer : DiagnosticAnalyzer
 	private void AnalyzeSymbol(SymbolAnalysisContext context)
 	{
 		// Get the symbol for the analyzed type or method
-		var symbol = context.Symbol;
+		INamedTypeSymbol symbol = (INamedTypeSymbol)context.Symbol;
+
+		if (symbol.DeclaringSyntaxReferences.All(static s => s.GetSyntax() is not ClassDeclarationSyntax c || c.IsPartial()))
+		{
+			return;
+		}
 
 		// Check if the symbol has any attributes
-		foreach (var attribute in symbol.GetAttributes())
+		foreach (AttributeData attribute in symbol.GetAttributes())
 		{
 			// Check if the attribute is the marker Attribute
-			if ((attribute.AttributeClass?.Name) != ModAnnotationGenerator.AttributeName || attribute.AttributeClass.ContainingNamespace.ToDisplayString() != ModAnnotationGenerator.AttributeNamespace)
+			if (attribute.AttributeClass?.Name is not ModAnnotationGenerator.AttributeName || attribute.AttributeClass.ContainingNamespace.ToDisplayString() != ModAnnotationGenerator.AttributeNamespace)
 			{
 				continue;
 			}
 
-			Location? location = attribute.ApplicationSyntaxReference?.GetSyntax().GetLocation();
-			if (location is null)
+			if (attribute.ApplicationSyntaxReference?.GetSyntax() is not AttributeSyntax attributeSyntax)
 			{
 				continue;
 			}
 
-			INamedTypeSymbol type = (INamedTypeSymbol)symbol;
-			if (type.DeclaringSyntaxReferences.All(s => s.GetSyntax() is ClassDeclarationSyntax c && c.IsPartial()))
+			if (attributeSyntax.Parent is not AttributeListSyntax attributeList || attributeList.Parent is not ClassDeclarationSyntax classDeclaration)
 			{
 				continue;
 			}
 
 			// Report a diagnostic if the marker Attribute is found
-			var diagnostic = Diagnostic.Create(Rule, location);
-			context.ReportDiagnostic(diagnostic);
+			context.ReportDiagnostic(Diagnostic.Create(Rule, classDeclaration.Identifier.GetLocation()));
 		}
 	}
 }
